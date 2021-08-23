fd980c5ef5bcaa70a371bfb0fa9931568c642de4
Fahmi Kudo <fahmi.hidayatullah12@gmail.com>
2021-08-23 21:27:32 +0700

    initiate project

---
+++ src/main/java/io/fahmikudo/wallet/MiniWalletApplication.java
@@ -0,0 +1,13 @@
+package io.fahmikudo.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/fahmikudo/wallet/MiniWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fahmikudo.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
